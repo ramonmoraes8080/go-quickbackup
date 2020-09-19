d756725878bba838fb65bb6823511d93ad64df1e
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-09-19 14:34:28 -0400

    Adjusting backend's folder hierarchy

--- backends/local/local.go
+++ backends/local/local.go
@@ -19,10 +19,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-)
+	"regexp"
 
-import (
-	"gitlab.com/velvetkeyboard/go-backup/utils"
+	"gitlab.com/velvetkeyboard/go-quickbackup/constants"
+	"gitlab.com/velvetkeyboard/go-quickbackup/utils"
 )
 
 type BackendLocalFilesystem struct {
@@ -47,6 +47,28 @@ func (b *BackendLocalFilesystem) Upload(zipFilePath string) {
 	}
 }
 
+func (b *BackendLocalFilesystem) List() []string {
+	// TODO Maybe we could present this list grouped by date
+	var ret []string
+	for _, filePath := range utils.WalkDir(b.Path) {
+		regexStr := fmt.Sprintf(
+			"%s-\\w+-\\d{4}-\\d{2}-\\d{2}-\\d{6}\\.%s",
+			constants.PREFIX, constants.EXTENSION,
+		)
+		if match, _ := regexp.MatchString(regexStr, filePath); match {
+			ret = append(ret, filePath)
+		}
+	}
+	return ret
+}
+
 // Copy file zip to an arbitrary path
-func (b *BackendLocalFilesystem) Download(zipFileName string, dest string) {
+func (b *BackendLocalFilesystem) Download(zipFilePath string, moveTo string) {
+	// TODO Check if the upload file exists?
+	_, zipFileName := filepath.Split(zipFilePath)
+	// err := os.Rename(zipFilePath, filepath.Join(moveTo, zipFileName))
+	_, err := utils.Copy(zipFilePath, filepath.Join(moveTo, zipFileName))
+	if err != nil {
+		panic(err) // TODO Should we panic here?
+	}
 }
