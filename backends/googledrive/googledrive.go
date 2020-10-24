733e8376b675b665ae70ac915552888f3decc192
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-10-24 10:55:06 -0400

    Backend Google Drive - Adding support to upload flow

--- backends/googledrive/googledrive.go
+++ backends/googledrive/googledrive.go
@@ -16,6 +16,11 @@ limitations under the License.
 package googledrive
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	gdrive_svc "gitlab.com/velvetkeyboard/go-quickbackup/services/googledrive"
 	"gitlab.com/velvetkeyboard/go-quickbackup/utils"
 )
@@ -34,20 +39,36 @@ func (b *BackendGoogleDrive) Init(path string, configPath string) {
 }
 
 func (b *BackendGoogleDrive) Upload(zipFilePath string) {
+	zipFileContent, err := os.Open(zipFilePath)
+
+	if err != nil {
+		panic(fmt.Sprintf("Cannot Open File %s: %v", zipFilePath, err))
+	}
+
+	zipFileName := filepath.Base(zipFilePath)
+
+	b.GoogleDriveSvc.UploadFile(
+		b.Path,
+		zipFileName,
+		zipFileContent,
+	)
 }
 
 func (b *BackendGoogleDrive) Download(zipFileName string, dest string) {
 	zipFileNameID := b.GoogleDriveSvc.GetFileId(zipFileName)
-	println("zipFileNameID", zipFileNameID)
-	b.GoogleDriveSvc.DownloadFile(zipFileNameID)
+	fileBytes := b.GoogleDriveSvc.DownloadFile(zipFileNameID)
+	err := ioutil.WriteFile(filepath.Join(dest, zipFileName), fileBytes, 0644)
+
+	if err != nil {
+		panic(err) // TODO Should we panic here?
+	}
 }
 
-func (b *BackendGoogleDrive) List() []string {
+func (b *BackendGoogleDrive) List(schemaName string) []string {
 	var ret []string
-	if files := b.GoogleDriveSvc.ListFilesFromFolder(b.Path); len(files) > 0 {
+	if files := b.GoogleDriveSvc.ListFilesFromFolder(schemaName, b.Path); len(files) > 0 {
 		for _, file := range files {
 			ret = append(ret, file.Name)
-			b.Download(file.Name, "/foo/bar")
 			break
 		}
 	}
